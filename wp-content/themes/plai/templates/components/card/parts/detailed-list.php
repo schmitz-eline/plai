9e29212f8ba9b968564c6bfa754541dd6e3fdae9
schmitz-eline <eline.schmitz@student.hepl.be>
2026-05-21 15:20:14 +0200

    feat: add missions page

--- wp-content/themes/plai/templates/components/card/parts/detailed-list.php
+++ wp-content/themes/plai/templates/components/card/parts/detailed-list.php
@@ -5,8 +5,10 @@
             $title = get_sub_field('card_detailed_list_title');
             $text = get_sub_field('card_detailed_list_text');
             ?>
-            <dt class="card__detailed-list__title card-secondary-text"><?= esc_html($title) ?></dt>
-            <dd class="card__detailed-list__text text"><?= esc_html($text) ?></dd>
+            <div class="card__detailed-list__item">
+                <dt class="card__detailed-list__title card-secondary-text"><?= esc_html($title) ?></dt>
+                <dd class="card__detailed-list__text text"><?= esc_html($text) ?></dd>
+            </div>
         <?php endwhile; ?>
     </dl>
 <?php endif; ?>
